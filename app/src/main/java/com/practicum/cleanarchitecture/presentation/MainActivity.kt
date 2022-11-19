489d8c8c8f5da942dd24263c6eb79fc3114d070d
Admin <bokinr@mail.ru>
2022-11-19 21:47:52 +0300

    Start

---
+++ app/src/main/java/com/practicum/cleanarchitecture/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.practicum.cleanarchitecture.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practicum.cleanarchitecture.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
